029247a878441c4df444388c3359b6b185ca0256
cmcoke <minoru.cm@gmail.com>
2025-03-06 15:05:36 -0500

    Corrected the TypeScript type for the handleResponse function in the paypal.ts file

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,96 +1,71 @@
-/**
- *
- */
-
-//
 const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-//
 export const paypal = {
-  //
   createOrder: async function createOrder(price: number) {
-    const accessToken = await generateAccessToken(); //
-    const url = `${base}/v2/checkout/orders`; //
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
 
-    //
     const response = await fetch(url, {
-      method: "POST", //
-      //
+      method: "POST",
       headers: {
-        "Content-Type": "application/json", //
-        Authorization: `Bearer ${accessToken}` //
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`
       },
-      //
       body: JSON.stringify({
-        intent: "CAPTURE", //
-        //
+        intent: "CAPTURE",
         purchase_units: [
           {
-            //
             amount: {
-              currency_code: "USD", //
-              value: price //
+              currency_code: "USD",
+              value: price
             }
           }
         ]
       })
     });
 
-    return handleResponse(response); //
+    return handleResponse(response);
   },
-
-  //
   capturePayment: async function capturePayment(orderId: string) {
-    const accessToken = await generateAccessToken(); //
-    const url = `${base}/v2/checkout/orders/${orderId}/capture`; //
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
 
-    //
     const response = await fetch(url, {
-      method: "POST", //
-      //
+      method: "POST",
       headers: {
-        "Content-Type": "application/json", //
-        Authorization: `Bearer ${accessToken}` //
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`
       }
     });
-
-    return handleResponse(response); //
+    return handleResponse(response);
   }
 };
 
-// Generate an access token for the PayPal API
+// Generate paypal access token
 async function generateAccessToken() {
-  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env; //
-
-  //
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
   const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString("base64");
 
-  //
   const response = await fetch(`${base}/v1/oauth2/token`, {
-    method: "POST", //
-    body: "grant_type=client_credentials", //
-    //
+    method: "POST",
+    body: "grant_type=client_credentials",
     headers: {
-      Authorization: `Basic ${auth}`, //
-      "Content-Type": "application/x-www-form-urlencoded" //
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded"
     }
   });
 
-  const jsonData = await handleResponse(response); //
-
-  return jsonData.access_token; //
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
 }
 
-//
-async function handleResponse(response: any) {
-  //
-  if (response.status === 200 || response.status === 201) {
-    return response.json(); //
+async function handleResponse(response: Response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
   }
-
-  const errorMessage = await response.text(); //
-
-  throw new Error(errorMessage); //
 }
 
 export { generateAccessToken };
